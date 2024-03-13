fc8947880fb6fc549610769cafb278e4947c95ef
Anastasia Morozova <anafrost1993@gmail.com>
2024-03-13 16:03:40 +0700

    chore: added image for start page

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,3 +1,6 @@
 export const API_URL = 'https://futures-api.poloniex.com/api/v2/tickers';
 
 export const QUOTES = ['a', 'b', 'c'];
+
+import cat from '@src/shared/images/cat-broker.jpg';
+export const CAT_BROKER = cat;
