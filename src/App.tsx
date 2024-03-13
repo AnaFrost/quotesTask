8ecad2551172ca430917e5a2496118c12c2af98c
Anastasia Morozova <anafrost1993@gmail.com>
2024-03-13 15:37:49 +0700

    chore: added styles for App

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { CssBaseline } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
 import Routing from './routes/Routing';
 
+import './App.css';
+
 const App = () => {
-  return (
-    <BrowserRouter>
-      <CssBaseline />
-      <Routing />
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<CssBaseline />
+			<Routing />
+		</BrowserRouter>
+	);
 };
 
 export default App;
